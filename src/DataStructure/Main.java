ea41dabe58a3d1b8d955538a6c0803a5a88536ca
alexgara <alexsanchg@protonmail.com>
2022-09-24 21:24:19 +0200

    Main.java files empty.

--- src/DataStructure/Main.java
+++ src/DataStructure/Main.java
@@ -3,10 +3,6 @@ package DataStructure;
 public class Main {
     public static void main(String[] args) {
 
-        Position pos1 = new Position(0,0);
-        Position pos2 = new Position(0,4);
-
-        System.out.println(pos1.inZone(pos2));
-        System.out.println(pos1.distance(pos2));
+        return;
     }
 }
