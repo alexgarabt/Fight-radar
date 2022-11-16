1897f9bedee2afe68c26ef34c470948d2f5f01fc
alexgara <alexsanchg@protonmail.com>
2022-11-16 09:46:30 +0100

    Testing.

--- New_fight-radar/src/Main.java
+++ New_fight-radar/src/Main.java
@@ -1,11 +1,12 @@
 import DataStructure.*;
 import DataStructure.Data.*;
 
-import javax.xml.crypto.Data;
 import java.io.*;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Implementation of the second part of the practice.
@@ -70,4 +71,66 @@ public class Main {
         if(!userList.containsKey(Id)){throw new Error("No existe ningún usuario con ese identificador");}
         return userList.get(Id);
     }
+
+    /**
+     * Return the users that are in the control zone of the given "user";
+     * <p>Efficiency: O(n)
+     *
+     * @param user The given user.
+     * @param userList A map of all the users.
+     * @return A Neighbours object with the users in the control zone.
+     */
+    public static Neighbours getNeighbours(DataUser user, HashMap<Id,DataUser> userList) {
+        ArrayList<String> vecs = new ArrayList<>();
+        Neighbours neighbours = new Neighbours();
+        DataUser userTmp = null;
+        //Iterate the Map
+        for(Map.Entry<Id,DataUser> set: userList.entrySet()) {// n (Iteraciones).
+            userTmp = set.getValue();
+            if (user.inZone(userTmp)) {
+                neighbours.add(userTmp.getId());//O(1)
+            }
+        }
+        return neighbours;
+    }
+
+
+
+
+
+
+
+
+    /**
+     * Creates the correct message for each case of move.
+     * @param user the user that has moved.
+     * @param userNeighbour the neighbour that enters or exits.
+     * @param typeNeighbour true==(if the neighbour enters), false==(if the neighbour exits)
+     * @return The correct message.
+     */
+    public String message(DataUser user, DataUser userNeighbour, boolean typeNeighbour){
+        String message =user.getId().toString();
+
+        if(typeNeighbour){//The userNeighbour enters
+            message=message+"+";
+            if(user.getGroup().equals(userNeighbour)) message=message+"=";//Same group.
+            else message=message+"/";//Different group.
+        }
+        else message=message+"-";//The userNeighbour exits.
+
+        message=message+userNeighbour.getId().toString();
+        return message;
+    }
+
+    public void main(String[] args){
+        Id id1 = new Id("hola");
+        Id id2 = new Id("hola");
+        Id id3 = new Id("adios");
+
+        Neighbours neighbours = new Neighbours();
+        neighbours.add(id1);
+        System.out.println(neighbours.isNeighbour(id1));
+    }
+
+
 }
